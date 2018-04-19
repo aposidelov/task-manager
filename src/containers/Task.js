e8bc8f3a27427037d5961a30d9b9560657c984d6
dead-drunk <posideloff@gmail.com>
2018-04-19 21:02:15 +0300

    Update Task.js

--- src/containers/Task.js
+++ src/containers/Task.js
@@ -16,9 +16,9 @@ import { filter } from '../utils';
 class Task extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {titleVal: ''};
+        this.state = { titleVal: '' };
 
-        const {match: {params: {taskId}}, selectTask, getTask} = props;
+        const {match: { params: { taskId } }, selectTask, getTask } = props;
         selectTask(taskId);
         this.task = getTask(taskId);
         
@@ -28,31 +28,31 @@ class Task extends React.Component {
     }
 
     startEdit (id) {
-        const {edit} = this.props;
-        this.setState({titleVal: ''});
+        const { edit } = this.props;
+        this.setState({ titleVal: '' });
         edit.start(id);
     }
    
-    changeTitle ({target: {value}}) {
-        this.setState({titleVal: value});
+    changeTitle ({ target: { value } }) {
+        this.setState({ titleVal: value });
     }
 
     submitHdr () {
-        const {titleVal} = this.state;
-        const {edit} = this.props;
-        if (titleVal) edit.submit({...this.task, name: titleVal});
+        const { titleVal } = this.state;
+        const { edit } = this.props;
+        if (titleVal) edit.submit({ ...this.task, name: titleVal });
     }
 
     componentWillReceiveProps (nextProps) {
-        const {match: {params: {taskId}}, editState, getTask} = nextProps;
+        const { match: { params: { taskId } }, editState, getTask } = nextProps;
         if (editState === 'success') this.task = getTask(taskId);
     }
     
     render() {
 
-        const {titleVal} = this.state;
-        const {match: {params: {taskId}}, goHome, editState} = this.props;
-        const {name, description} = this.task;
+        const { titleVal } = this.state;
+        const { match: { params: { taskId } }, goHome, editState } = this.props;
+        const { name, description } = this.task;
                
         let nameFild = '',
             button = '';
@@ -63,15 +63,15 @@ class Task extends React.Component {
                 nameFild = (
                     <input 
                         type="text" 
-                        value= {titleVal}
-                        onChange= {e => this.changeTitle(e)} 
-                        style= {{width: '70%'}} 
+                        value={titleVal}
+                        onChange={ e => this.changeTitle(e) } 
+                        style={{ width: '70%' }} 
                     />
                 );
                 button = (
                     <button 
-                        onClick= {() => this.submitHdr()}
-                        style= {{float: 'right'}} 
+                        onClick={ () => this.submitHdr() }
+                        style={{ float: 'right' }} 
                     >
                         Edit
                     </button>
@@ -84,7 +84,7 @@ class Task extends React.Component {
             break;
           
             default:
-                nameFild = (<h4 onClick = {() =>  this.startEdit(taskId)}>{name}</h4>);
+                nameFild = (<h4 onClick={ () =>  this.startEdit(taskId) }>{name}</h4>);
                 button = '';
         }    
 
@@ -93,11 +93,11 @@ class Task extends React.Component {
         return (
 
             <Grid>
-                <h3 onClick = {goHome}><ArrowLeft /></h3>
+                <h3 onClick={goHome}><ArrowLeft /></h3>
                 <Row>
                     <Col md={2} xs={2}></Col>
                     <Col md={8} xs={8}>
-                        <Panel header = {headerContent}>
+                        <Panel header={headerContent}>
                             {description}
                         </Panel>    
                     </Col>
@@ -109,7 +109,7 @@ class Task extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-    const {tasks, taskHandling: {editState}} = state;
+    const { tasks, taskHandling: { editState } } = state;
     return {
         getTask: id => filter(tasks, id),
         editState

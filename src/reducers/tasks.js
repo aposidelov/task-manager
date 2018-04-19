5558c79431201741b5a6a7647fa26c145ccd57e0
dead-drunk <posideloff@gmail.com>
2018-04-19 21:09:19 +0300

    Update tasks.js

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -9,7 +9,6 @@ import {
 } from '../actions/types';
 
 
-
 const initialTasks = mock.map((item) => {
     const newItem = {...item};
     newItem.id = '' + item.id;
@@ -20,7 +19,7 @@ export const tasks = (state = initialTasks, action) => {
 
     switch (action.type) {
         case SENT_SUCCESS:
-            const {id, name} = action.payload;
+            const { id, name } = action.payload;
             return state.map((item) => {
                 const newItem = {...item};
                 if(item.id === id) newItem.name = name;
@@ -42,16 +41,16 @@ export const taskHandling = (state = handlingInitial, action) => {
 
     switch (action.type) {
         case SELECT_TASK:
-            return {...state, id: action.id};
+            return { ...state, id: action.id };
         case WILL_EDIT_NAME:
-            return {...state, ...action.payload};
+            return { ...state, ...action.payload };
         case SEND_DATA:
-            return {...state, editState: 'sending'};
+            return { ...state, editState: 'sending' };
         case SENT_SUCCESS:
-            const {id, name: value} = action.payload;
-            return {...state, editState: 'success', id, value}; 
+            const { id, name: value } = action.payload;
+            return { ...state, editState: 'success', id, value }; 
         case END_EDIT:
-            return {...state, ...action.payload};
+            return { ...state, ...action.payload };
                     
         default: return state
     }
@@ -70,11 +69,11 @@ export const sendTask = (state = sendTaskInitial, action) => {
 
     switch (action.type) {
         case SEND_DATA:
-            return {...state, ...action.payload};
+            return { ...state, ...action.payload };
         case SENT_SUCCESS:
-            return {...state, ...action.payload};
+            return { ...state, ...action.payload };
         case SENT_FAILURE:
-            return {...state, ...action.payload}; 
+            return { ...state, ...action.payload }; 
 
         default: return state
     }
